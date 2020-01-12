8a4394869558e91d681288569e0c834867fbc06e
tjsha_lfek1mv <tjsha_lfek1mv@Tharmai7>
2020-01-11 19:54:16 -0500

    HelloMiddleEarth class created.

---
+++ src/test/java/HelloMiddleEarth.java
@@ -0,0 +1,14 @@
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		
+		// print greeting
+		for(String name: names) {
+			System.out.println("Hello " + name);
+		}
+
+	}
+
+}
