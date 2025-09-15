dbf1d60458cd307dbe1ce37967f81d19170bdb2f
tjshankar123 <37399541+tjshankar123@users.noreply.github.com>
2025-09-15 15:24:14 -0400

    Merge remote-tracking branch 'origin/master'
    Amended by TJ

--- src/test/java/GitDemo/Login.java
+++ src/test/java/GitDemo/Login.java
@@ -10,7 +10,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Login {
-    // Auther: TJ
+    // Author: TJ
 
     WebDriver driver;
     WebElement element;
